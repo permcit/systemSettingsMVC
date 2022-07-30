9bdf911a07f7c4457cfb172fe7355f5ca1bf6eef
Roman Nazarov <ste7ww@gmail.com>
2022-07-30 15:00:03 +0500

    Initial Commit

---
+++ systemSettingsMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  systemSettingsMVC
+//
+//  Created by Роман Назаров on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
